f9724a1146d4b8cf57d273454c7e382ef060e749
IGOR_CAIXETA <igorsouzalima01@gmail.com>
2022-10-06 16:40:23 -0300

    corrigi o nome da companhia area para aerea

--- book/tests.py
+++ book/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
 
 # Create your tests here.
+from emprestimo_livros.models import Livro, Usuario, Emprestimo
+class VooTests(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        companhia_area.objects.create()
+        Voo.objects.create(companhia_area='TAM',aeroporto_destino='Congonhas-SP',codigo_de_voo='')
+    def test_criacao_id(self):
+        livro_1 = Livro.objects.get(titulo='Os Irmãos Karamazov')
+        self.assertEqual(livro_1.id, 1)
