cc820907f5967e228773a65235ba3f60da321e43
Luiz Mota <luizmota@alunos.utfpr.edu.br>
2022-10-06 17:37:41 -0300

    Implementação inicial dos testes

--- book/tests.py
+++ book/tests.py
@@ -1,12 +1,24 @@
 from django.test import TestCase
-
+import datetime
 # Create your tests here.
-from emprestimo_livros.models import Livro, Usuario, Emprestimo
+from book.models import Voo,Operador,Funcionario,Piloto,Status,Companhia
 class VooTests(TestCase):
     @classmethod
     def setUpTestData(cls):
-        companhia_area.objects.create()
-        Voo.objects.create(companhia_area='TAM',aeroporto_destino='Congonhas-SP',codigo_de_voo='')
+        Funcionario.objects.create(nome='Igor',email='igor@usp.br',salario=3,endereco='Rua de Passos')
+        Piloto.objects.create(nome='Igor',email='igor@usp.br',salario=3,endereco='Rua de Passos')
+        Status.objects.create(proximo_status=1,horario=datetime.date.today())
+        funcionario_1 = Funcionario.objects.get(nome='Igor')
+        piloto_1 = Piloto.objects.get(nome='Igor')
+        Voo.objects.create(
+            aeroporto_destino='Congonhas-SP',
+            aeroporto_partida='Passos-MG',
+            partida_prevista=datetime.date(2022,6,10),
+            chegada_prevista=datetime.date(2022,6,11),
+            status=1
+            )
+        voo_1 = Voo.objects.get(aeroporto_destino='Congonhas-SP')
+        Companhia.objects.create(nome='TAM',funcionarios_id=funcionario_1,pilotos_id=piloto_1,voo_id=voo_1)
     def test_criacao_id(self):
-        livro_1 = Livro.objects.get(titulo='Os Irmãos Karamazov')
-        self.assertEqual(livro_1.id, 1)
+        voo_1 = Voo.objects.get(aeroporto_destino='Congonhas-SP')
+        self.assertEqual(voo_1.id, 1)
