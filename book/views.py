755737c88439a57b824598771bc8632b38a580f4
Luiz Mota <luizmota@alunos.utfpr.edu.br>
2022-10-13 15:33:33 -0300

    Adicionando telas

--- book/views.py
+++ book/views.py
@@ -2,5 +2,17 @@ from django.shortcuts import render
 
 # Create your views here.
 
-def bookview(request):
-    return render(request, "first.html")
+def login(request):
+    return render(request, "login.html")
+
+def inicial(request):
+    return render(request, "inicial.html")
+
+def cadastroVoos(request):
+    return render(request, "cadastroVoos.html")
+
+def relatorios(request):
+    return render(request, "relatorios.html")
+
+def monitoraVoos(request):
+    return render(request, "monitoraVoos.html")
