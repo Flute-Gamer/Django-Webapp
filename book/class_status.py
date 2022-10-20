00e37d4e45b31317b9ef5344052c49549a0d190c
IGOR_CAIXETA <igorsouzalima01@gmail.com>
2022-10-20 15:24:49 -0300

    Criada classe status

---
+++ book/class_status.py
@@ -0,0 +1,26 @@
+class Status:
+    ##Construir Classe
+    def __init__(proximo_status, status_atual):
+        self.status_atual    = 0
+        self.proximo_status  = proximo_status
+        self.horario         = 0
+
+    ##GETTERS
+    def get_status_atual(self):
+        return self.status_atual
+
+    def get_proximo_status(self):
+        return self.proximo_status
+
+    def get_horario(self):
+        return self.horario
+
+    ##SETTERS
+    def set_status_atual(self, status_atual):
+        self.status_atual = status_atual
+
+    def set_proximo_status(self, proximo_status):
+        self.proximo_status = proximo_status
+
+    def set_email(self, horario):
+        self.horario = horario
