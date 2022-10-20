88f95ecf60dfe7505f5e4a1abb3683eb644ae456
IGOR_CAIXETA <igorsouzalima01@gmail.com>
2022-10-20 15:20:09 -0300

    Criada classe Piloto :globe_with_meridians:

---
+++ book/class_piloto.py
@@ -0,0 +1,40 @@
+class Piloto:
+    ##Construir Classe
+    def __init__(self, nome, cpf, email, salario, endereco):
+        self.nome     = nome
+        self.cpf      = cpf
+        self.email    = email
+        self.salario  = salario
+        self.endereco = endereco
+
+    ##GETTERS
+    def get_nome(self):
+        return self.nome
+
+    def get_cpf(self):
+        return self.cpf
+
+    def get_email(self):
+        return self.email 
+
+    def get_salario(self):
+        return self.salario
+
+    def get_endereco(self):
+        return self.endereco
+
+    ##SETTERS
+    def set_nome(self, nome):
+        self.nome = nome
+
+    def set_cpf(self, cpf):
+        self.cpf = cpf
+
+    def set_email(self, email):
+        self.email = email
+
+    def set_salario(self, salario):
+        self.salario = salario
+
+    def set_email(self, endereco):
+        self.endereco = endereco
