e1e03e6ab27228a1eae4bd6b1220ad0e98436b11
IGOR_CAIXETA <igorsouzalima01@gmail.com>
2022-10-20 14:51:10 -0300

    esqueleto classe_voo

--- book/class_voo.py
+++ book/class_voo.py
@@ -1,5 +1,36 @@
 class Voo()
 
-def __init__(self, codigo_de_voo, status, aeroporto_destino, aeroporto_partida, partida_prevista, partida_real, chegada_prevista, chegada_real)
+    def __init__(self, status, aeroporto_destino, aeroporto_partida, partida_prevista, chegada_prevista)
+        ##Construir Classe
+        self.codigo_de_voo     = 0
+        self.status            = status
+        self.aeroporto_destino = aeroporto_destino
+        self.aeroporto_partida = aeroporto_partida
+        self.partida_prevista  = partida_prevista
+        self.partida_real      = 0
+        self.chegada_prevista  = chegada_prevista
+        self.chegada_real      = 0
 
-##Construir Classe
+    def get_codigo_de_voo
+        return codigo_de_voo
+
+    def get_status
+        return status
+        
+    def get_aeroporto_destino
+        return aeroporto_destino
+
+    def get_aeroporto_partida
+        return aeroporto_partida
+
+    def get_partida_prevista
+        return partida_prevista
+
+    def get_partida_real
+        return partida_real
+
+    def get_chegada_prevista
+        return chegada_prevista
+
+    def get_chegada_real
+        return chegada_real      
