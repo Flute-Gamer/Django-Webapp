a55edb14bd53f65cf16b582cd25c2f1b99c2debb
Luiz Mota <luizmota@alunos.utfpr.edu.br>
2022-11-10 15:09:20 -0300

    Alterações mínimas relatorio

--- book/views.py
+++ book/views.py
@@ -1,5 +1,6 @@
+import io
 from django.shortcuts import render
-
+from django.http import FileResponse
 # Create your views here.
 
 def login(request):
@@ -12,7 +13,14 @@ def cadastroVoos(request):
     return render(request, "cadastroVoos.html")
 
 def relatorios(request):
+    retornaRelatorioPDF(request)
     return render(request, "relatorios.html")
 
 def monitoraVoos(request):
     return render(request, "monitoraVoos.html")
+
+
+def retornaRelatorioPDF(request):
+    buffer = io.BytesIO()
+    buffer.seek(0)
+    return FileResponse(buffer, as_attachment=True, filename='requirements.txt')
