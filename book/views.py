2b99af108fc2f12ccb74562d87c2242e6d215b1d
Luiz Mota <luizmota@alunos.utfpr.edu.br>
2022-11-16 14:40:51 -0300

    Corrige Voo mostrado

--- book/views.py
+++ book/views.py
@@ -27,25 +27,20 @@ def cadastroVoos(request):
         form = Formulario_Cadastro_Voo(request.POST)
         if form.is_valid():
             print (form.cleaned_data)
-            # form = Formulario_Cadastro_Voo()
             codigo = form.cleaned_data['código_do_voo']
             destino = form.cleaned_data['destino_do_voo']
             origem = form.cleaned_data['origem_do_voo'] 
             partida_prev = form.cleaned_data['partida_prevista'] 
             chegada_prev = form.cleaned_data['chegada_prevista'] 
             print('DESTINO: ' + str(destino))
-            
-            if(Voo.objects.get(codigo) is None):
-                Voo.objects.create(
-                codigo_de_voo=codigo,
-                aeroporto_destino=destino,
-                aeroporto_partida=origem,
-                partida_prevista=partida_prev,
-                chegada_prevista=chegada_prev,
-                status=1
-                )
-            else:
-                pass
+            Voo.objects.create(
+            codigo_de_voo=codigo,
+            aeroporto_destino=destino,
+            aeroporto_partida=origem,
+            partida_prevista=partida_prev,
+            chegada_prevista=chegada_prev,
+            status=1
+            )
                 #Refresh
                 #Error msg
     
@@ -74,43 +69,43 @@ def monitoraVoos(request):
 
 def mostra_codigo_do_voo(codigo_voo):
     voo = Voo.objects.get(codigo_de_voo=codigo_voo)
-    print('Código do voo: '+ str(voo.codigo_de_voo()))
-    return ('Código do voo: '+ str(voo.codigo_de_voo()))
+    print('Código do voo: '+ str(voo.codigo_de_voo))
+    return ('Código do voo: '+ str(voo.codigo_de_voo))
 
 def mostra_status_do_voo(codigo_voo):
     voo = Voo.objects.get(codigo_de_voo=codigo_voo)
-    print('Status do voo: '+ str(voo.status()))
-    return ('Status do voo: '+ str(voo.status()))
+    print('Status do voo: '+ str(voo.status))
+    return ('Status do voo: '+ str(voo.status))
 
 def mostra_aeroporto_destino(codigo_voo):
     voo = Voo.objects.get(codigo_de_voo=codigo_voo)
-    print('Destino do voo: '+ str(voo.aeroporto_destino()))
-    return ('Destino do voo: '+ str(voo.aeroporto_destino()))
+    print('Destino do voo: '+ str(voo.aeroporto_destino))
+    return ('Destino do voo: '+ str(voo.aeroporto_destino))
 
 def mostra_aeroporto_partida(codigo_voo):
     voo = Voo.objects.get(codigo_de_voo=codigo_voo)
-    print('Partida do voo: '+ str(voo.aeroporto_partida()))
-    return ('Partida do voo: '+ str(voo.aeroporto_partida()))
+    print('Partida do voo: '+ str(voo.aeroporto_partida))
+    return ('Partida do voo: '+ str(voo.aeroporto_partida))
 
 def mostra_partida_prevista(codigo_voo):
     voo = Voo.objects.get(codigo_de_voo=codigo_voo)
-    print('Partida prevista: '+ str(voo.partida_prevista()))
-    return ('Partida prevista: '+ str(voo.partida_prevista()))
+    print('Partida prevista: '+ str(voo.partida_prevista))
+    return ('Partida prevista: '+ str(voo.partida_prevista))
 
 def mostra_partida_real(codigo_voo):
     voo = Voo.objects.get(codigo_de_voo=codigo_voo)
-    print('Partida real: '+ str(voo.partida_real()))
-    return ('Partida real: '+ str(voo.partida_real()))
+    print('Partida real: '+ str(voo.partida_real))
+    return ('Partida real: '+ str(voo.partida_real))
 
 def mostra_chegada_prevista(codigo_voo):
     voo = Voo.objects.get(codigo_de_voo=codigo_voo)
-    print('Chegada prevista: '+ str(voo.chegada_prevista()))
-    return ('Chegada prevista: '+ str(voo.chegada_prevista()))
+    print('Chegada prevista: '+ str(voo.chegada_prevista))
+    return ('Chegada prevista: '+ str(voo.chegada_prevista))
 
 def mostra_chegada_real(codigo_voo):
     voo = Voo.objects.get(codigo_de_voo=codigo_voo)
-    print('Chegada real: '+ str(voo.chegada_real()))
-    return ('Chegada real: '+ str(voo.chegada_real()))
+    print('Chegada real: '+ str(voo.chegada_real))
+    return ('Chegada real: '+ str(voo.chegada_real))
 
 
 def retornaRelatorioPDF(request):
