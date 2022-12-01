6d3c66a1081a4be02c9648c175fa03e66d0b419c
Luiz Mota <luizmota@alunos.utfpr.edu.br>
2022-12-01 09:31:35 -0300

    Atualiza tudo para DateTimeField

--- book/forms.py
+++ book/forms.py
@@ -5,25 +5,25 @@ class Formulario_Cadastro_Voo(forms.Form):
     código_do_voo = forms.IntegerField(min_value=0)
     destino_do_voo = forms.CharField()
     origem_do_voo = forms.CharField()
-    partida_prevista = forms.DateField(error_messages={'invalid': "Esta data é inválida!"})
-    chegada_prevista = forms.DateField(error_messages={'invalid': "Esta data é inválida!"})
+    partida_prevista = forms.DateTimeField(error_messages={'invalid': "Esta data é inválida!"})
+    chegada_prevista = forms.DateTimeField(error_messages={'invalid': "Esta data é inválida!"})
 
 class Formulario_Atualiza_Voos(forms.Form):
     código_do_voo = forms.IntegerField(min_value=0)
     destino_do_voo = forms.CharField(required = False)
     origem_do_voo = forms.CharField(required = False)
-    partida_prevista = forms.DateField(error_messages={'invalid': "Esta data é inválida!"}, required = False)
-    chegada_prevista = forms.DateField(error_messages={'invalid': "Esta data é inválida!"}, required = False)
-    partida_real = forms.DateField(error_messages={'invalid': "Esta data é inválida!"}, required = False)
-    chegada_real = forms.DateField(error_messages={'invalid': "Esta data é inválida!"}, required = False)
+    partida_prevista = forms.DateTimeField(error_messages={'invalid': "Esta data é inválida!"}, required = False)
+    chegada_prevista = forms.DateTimeField(error_messages={'invalid': "Esta data é inválida!"}, required = False)
+    partida_real = forms.DateTimeField(error_messages={'invalid': "Esta data é inválida!"}, required = False)
+    chegada_real = forms.DateTimeField(error_messages={'invalid': "Esta data é inválida!"}, required = False)
     status = forms.IntegerField(min_value=1,required=False)
 
 class Formulario_Atualiza_Basico_Voos(forms.Form):
     código_do_voo = forms.IntegerField(min_value=0)
     destino_do_voo = forms.CharField(required = False)
     origem_do_voo = forms.CharField(required = False)
-    partida_prevista = forms.DateField(error_messages={'invalid': "Esta data é inválida!"}, required = False)
-    chegada_prevista = forms.DateField(error_messages={'invalid': "Esta data é inválida!"}, required = False)
+    partida_prevista = forms.DateTimeField(error_messages={'invalid': "Esta data é inválida!"}, required = False)
+    chegada_prevista = forms.DateTimeField(error_messages={'invalid': "Esta data é inválida!"}, required = False)
 
 class Codigo_Voo_Monitora(forms.Form):
     codigo_voo = forms.IntegerField(min_value=0,required=False)
